2cae4473013767c01e9c68985db78bfdf7dbeaf8
Eurico Paes <euricopaes@gmail.com>
2024-06-12 12:24:38 -0300

    new

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-import random
+
 import eventlet
 
 eventlet.monkey_patch()
-
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import random
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*")
