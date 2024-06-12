4e9b2f68bebbb5865a626a37928dbed7e7c64f13
Eurico Paes <euricopaes@gmail.com>
2024-06-11 21:22:56 -0300

    new

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,24 @@
-FROM python:3.11
+# Use the official Python image from the Docker Hub
+FROM python:3.8-slim
 
+# Set environment variables to prevent Python from writing pyc files and buffering stdout and stderr
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
 
+# Set the working directory
 WORKDIR /app
-COPY . ./
 
-RUN pip install -r requirements.txt
+# Copy the requirements file into the working directory
+COPY requirements.txt /app/
 
+# Install the dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code into the working directory
+COPY . /app/
+
+# Expose the port the app runs on
 EXPOSE 8080
 
-# Define environment variable
-ENV PORT 8080
-
-# Run app.py when the container launches
-# CMD python app.py
-
-# Run gunicorn with gevent when the container launches
-#CMD ["gunicorn", "-k", "gevent", "-w", "1", "-b", "0.0.0.0:8080", "app:app"]
-
-CMD ["gunicorn", "-k", "geventwebsocket.gunicorn.workers.GeventWebSocketWorker", "-w", "5", "-b", ":8080", "app:app"]
+# Run the application
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,104 +1,31 @@
-import gevent.monkey
-gevent.monkey.patch_all()
-from flask import Flask, render_template, url_for
-import websocket
-import json
-import pandas as pd
-from flask_socketio import SocketIO, send
-from threading import Lock
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 import random
-import os
+import eventlet
 
-
-endpoint = 'wss://stream.binance.com:9443/ws/!miniTicker@arr'
-
-thread = None
-thread_lock = Lock()
+eventlet.monkey_patch()
 
 app = Flask(__name__)
-#socketio = SocketIO(app, cors_allowed_origins='*')
-#socketio = SocketIO(app, async_mode='gevent',cors_allowed_origins='*')
-socketio = SocketIO(app, async_mode='gevent', logger=True, engineio_logger=True,cors_allowed_origins='*')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*")
 
-def df_import(data):
-    global btc
-    global times
-    #Creating DF
-    df_ = pd.DataFrame(data)
-    #Filtering BTC USDT
-    df_ = df_[df_['s'].str.endswith('BTCUSDT')]
-    #Convert to float
-    df_.c = df_.c.astype(float)
-    #Convert to int - da erro - temos que converter para float
-    df_.E = df_.E.astype(float)
-    #df_.E = df_.E.astype(int)
-    #df_.E = pd.to_datetime(df_['E'],unit='ms')
-    #Selecting Columns
-    final = df_[['s','E','c']]
-    #Print
-    print(final.iloc[-1].c)
-    btc = final.iloc[-1].c
-    times = final.iloc[-1].E
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    socketio.emit('updateData', {'btc': btc, 'times':times})
-    #socketio.emit('updateData', {'btc': btc})
-    #socketio.emit('updateData', {'times':times})
-    #print(times)
-   
-
-
-def on_message(wd,message):
-    global out
-    out = json.loads(message)
-    df_import(out)
-
-@app.route('/', methods=['GET', 'POST'])
-def principal():
-
-	return render_template("linecharts.html")
-
-@app.route('/linecharts')
-def linecharts():
-	return render_template("linecharts.html")
-
-
-def background_thread():
-   
-    while True:
-        ws = websocket.WebSocketApp(endpoint, on_message=on_message)
-        ws.run_forever()
-
-    
-
-        #socketio.sleep(0.1)
-
-
-
-"""
-Decorator for connect
-"""
 @socketio.on('connect')
-def connect():
+def handle_connect():
     print('Client connected')
-    global thread
-    with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(background_thread)
+    emit('message', {'data': 'Connected to the server'})
 
-"""
-Decorator for disconnect
-"""
 @socketio.on('disconnect')
-def disconnect():
+def handle_disconnect():
     print('Client disconnected')
 
-
+@socketio.on('request_data')
+def handle_request_data():
+    data = {'value': random.randint(0, 100)}
+    emit('data_response', data)
 
 if __name__ == '__main__':
-    #socketio.run(app)
-    #socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-    #socketio.run(app, port=int(os.environ.get('PORT', 8080)))
-    #socketio.run(app, host='0.0.0.0', port=8080)
-    socketio.run(app, port=8080)
-	#app.run(debug=True)
-	#app.run(debug=False,port=8080,host="0.0.0.0")
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
